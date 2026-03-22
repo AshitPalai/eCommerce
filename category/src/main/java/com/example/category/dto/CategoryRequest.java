3f341f523349506d67535edf586a382d0e54b019
AshitPalai <ashitpalai1206@gmail.com>
2026-03-23 00:00:47 +0530

    till now i have added upto dto2

---
+++ category/src/main/java/com/example/category/dto/CategoryRequest.java
@@ -0,0 +1,11 @@
+package com.example.category.dto;
+
+
+import lombok.Data;
+
+@Data
+public class CategoryRequest {
+
+    private String name;
+    private String description;
+}
---
+++ category/src/main/java/com/example/category/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.example.category.repository;
+
+import com.example.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+}
