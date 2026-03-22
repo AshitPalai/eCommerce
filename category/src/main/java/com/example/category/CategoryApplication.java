cf773f71a3c27599d577acc531a40cf278cd82ce
AshitPalai <ashitpalai1206@gmail.com>
2026-03-22 23:59:38 +0530

    till now i have added upto dto

--- category/src/main/java/com/example/category/CategoryApplication.java
+++ category/src/main/java/com/example/category/CategoryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CategoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CategoryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryApplication.class, args);
+    }
 
 }
